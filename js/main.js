0def41d3f24cddd161f1bc8b7c8b804ccc1e35c3
sank-wot <aserobabow95@mail.ru>
2018-07-18 19:07:59 +0600

    init site

---
+++ js/main.js
@@ -0,0 +1,4 @@
+var header = document.getElementById('nav-btn');
+TweenMax.to(header, 1, {opacity: 100, x:150}); 
+var social = document.getElementById('social');
+TweenMax.to(social, 1, {opacity: 100, x:150}); 
